9a4d24a8beb0896e8529a37b52254cc2d527d853
Hasbulla20 <144480181+Hasbulla20@users.noreply.github.com>
2023-09-11 01:08:36 -0700

    Update Operacion.java

--- src/main/java/mx/itson/brocoli/negocio/Operacion.java
+++ src/main/java/mx/itson/brocoli/negocio/Operacion.java
@@ -10,7 +10,15 @@ package mx.itson.brocoli.negocio;
  */
 public class Operacion {
     //holaestoesun cambioL
-    
+       /**
+     * Separa una cadena de texto a partir de la expresión guion medio.
+     * @param valor Cadena de texto que será separada a partir de a expresion dada.
+     * @return array de string en donde cada posiucuon representa una division de a cadena de texto.
+     */
+    public String [] separar (String valor){
+        //hola esto es un cambio jejejejeejejeje
+        String [] elementos = valor.split("-");
+        return elementos;
     String[] elementos = valor.split("");
     
     
